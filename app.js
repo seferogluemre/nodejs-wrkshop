79b823296d1bf888da2ad8c0e7db68f3044105f0
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-21 12:07:35 +0300

    file system module 1

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const url = require('url');
+// const url = require('url');
 
-const address = "http://sadikturan.com/kurslar?year=2019&month=nisan"
+// const address = "http://sadikturan.com/kurslar?year=2019&month=nisan"
 
-let result = url.parse(address, true);
+// let result = url.parse(address, true);
 
-// Url adresinden ekledigimiz sorgu parametrelerini aldık
-console.log(result)
-console.log(result.path)
-console.log(result.query.year)
-console.log(result.query.month)
+// // Url adresinden ekledigimiz sorgu parametrelerini aldık
+// console.log(result)
+// console.log(result.path)
+// console.log(result.query.year)
+// console.log(result.query.month)
+
+
+const fs = require('fs');
+
+const files = fs.readdir('.../', function (error, data) {
+    console.log(error ? error : data)
+})
+

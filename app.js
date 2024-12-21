24486153cf1249465ccf7c1960eea87fe1c2146d
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-21 12:22:20 +0300

    logger nesnesinin oluşturulması ve eventemmitin kullanılması

--- app.js
+++ app.js
@@ -23,6 +23,25 @@ const fs = require('fs');
 //     console.log(error ? error : data);
 // })
 
-fs.writeFile('deneme.txt', 'Dosyanız oluşturuldu Emre SEFEROĞLU', function (error) {
-    console.log(error ? error : "Dosyanız oluşturuldu");
+// fs.writeFile('deneme.txt', 'Dosyanız oluşturuldu Emre SEFEROĞLU', function (error) {
+//     console.log(error ? error : "Dosyanız oluşturuldu");
+// })
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on("logout", function () {
+    console.log("Baglantı koptu")
 })
+
+emitter.on('connection', function (args) {
+    console.log("Baglantı " + args.message)
+    console.log("Baglantı numarası: " + args.id)
+})
+
+const Logger = require('./logger');
+const { log } = require('console');
+const logger = new Logger();
+
+logger.log('Emre SEFEROĞLU Giriş yaptı')
+
